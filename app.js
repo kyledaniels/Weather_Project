7f637d3673298c55c160a9d63db232b3b91d90e7
kyle67 <daniels.kyle@gmail.com>
2021-05-30 14:59:02 -0400

    Added images and text to page using res.write

--- app.js
+++ app.js
@@ -12,12 +12,17 @@ app.get("/", (req,res)=>{
         response.on("data",(data)=>{
            const weatherData = JSON.parse(data);
            const temp =  weatherData.main.temp
-           console.log(temp)
            const description = weatherData.weather[0].description
-           console.log(description);
+           const icon = weatherData.weather[0].icon
+           const imageURL =  "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+          
+           res.write("<p>The weather is currently " + description+"</p>");
+           res.write("<h1>The temperature in Hampton is " + temp + " degrees Celcius</h1>");
+           res.write("<img src=" + imageURL + ">");
+           res.send();
         })
     })
-    res.send("Server is up and running");
+    
 })
 
 
